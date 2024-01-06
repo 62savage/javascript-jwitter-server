723df7d986305604cfeb377c4e54065339ececd6
62savage <6s2avage@gmail.com>
2024-01-06 23:28:34 +0900

    feat(model): Auth로직과 관련된 Model 계층 생성

---
+++ data/auth.js
@@ -0,0 +1,20 @@
+let users = [
+  {
+    id: "1",
+    username: "bob",
+    password: "$2b$12$NzYKFhfO1EAILrmapW/ZMuT2GEECKswk/lt29N6aNPlY08MuiUVqq",
+    name: "Bob",
+    email: "bob@gmail.com",
+    url: "asdf",
+  },
+];
+
+export async function findByUsername(username) {
+  return users.find((user) => user.username === username);
+}
+
+export async function createUser(user) {
+  const created = { ...user, id: Date.now().toString() };
+  users.push(created);
+  return created.id;
+}
