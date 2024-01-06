7d10af9f75555445289a3124db644fc7b16a4e8e
62savage <6s2avage@gmail.com>
2024-01-06 23:27:43 +0900

    feat(controller): Auth로직과 관련된 Controller 계층 생성

---
+++ controller/auth.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import {} from "express-async-errors";
+import * as userRepository from "../data/auth.js";
+
+export async function signup(req, res) {
+  const { username, password, name, email, url } = req.body;
+  const found = await userRepository.findByUsername(username);
+  if (found) {
+    return res.status(409).json({ message: `${username} already exists` });
+  }
+  const hashed = await bcrypt.hash(
+    password,
+    Number(process.env.BCRYPT_SALT_ROUNDS)
+  );
+  const userId = await userRepository.createUser({
+    username,
+    password: hashed,
+    name,
+    email,
+    url,
+  });
+  const token = createJwtToken(userId);
+  res.status(201).json({ token, username });
+}
+
+export async function login(req, res) {
+  const { username, password } = req.body;
+  const user = await userRepository.findByUsername(username);
+  if (!user) {
+    return res.status(401).json({ message: "Invalid user or password" });
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    return res.status(401).json({ message: "Invalid user or password" });
+  }
+  const token = createJwtToken(user.id);
+  res.status(200).json({ token, username });
+}
+
+function createJwtToken(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPRIES_IN_DAYS,
+  });
+}
